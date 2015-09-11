a1c90cd53322f34b0a63310c468cccb5ff43f8fd
Atticus White <contact@atticuswhite.com>
2015-09-10 23:45:33 -0400

    build updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
     less = require('gulp-less');
 
 gulp.task('js', function () {
   gulp.src('src/**/*.js')
+  .pipe(rename('bernie-calendar.js'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('js:min', function () {
+  gulp.src('src/**/*.js')
   .pipe(uglify())
+  .pipe(rename('bernie-calendar.min.js'))
   .pipe(gulp.dest('dist/'));
 });
 
@@ -14,5 +22,9 @@ gulp.task('less', function () {
   .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['js', 'less']);
+gulp.task('build', ['js', 'js:min', 'less']);
 gulp.task('default', ['build']);
+gulp.task('watch', ['build'], function () {
+  gulp.watch('src/**/*.js', ['js']);
+  gulp.watch('src/**/*.less', ['less']);
+});
